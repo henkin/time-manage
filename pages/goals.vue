1e11f543b0c50c102ba6bb77eefa435b620f011b
Pavel Henkin <pavel.henkin@gmail.com>
2017-11-15 21:06:41 -0800

    store growing

--- pages/goals.vue
+++ pages/goals.vue
@@ -1,60 +1,60 @@
 <template>
-  <section class="container-fluid">
-    <div style="width: 100%">
+    <section class="container-fluid">
+        <div style="width: 100%">
 
-      <!--<logo/>-->
-      <div class="row">
-        <div class="col-sm">
-          <h1 class="title">
-            Goals
-          </h1>
+            <!--<logo/>-->
+            <div class="row">
+                <div class="col-sm">
+                    <h1 class="title">
+                        Goals
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <span>New goal:</span>
+
+                    <div class="goal-title-container">
+                        <form class="form-inline goal-form"
+                              v-on:submit.prevent="createGoal"
+                              autocomplete="off">
+                            <div class="input-group">
+                                <label class="sr-only" for="inlineFormInput">Name</label>
+                                <!-- these are to remove the autocomplete from Chrome -->
+                                <input style="display:none" type="text" name="fakeusernameremembered"/>
+                                <input style="display:none" type="password" name="fakepasswordremembered"/>
+                                <input v-model="goalName"
+                                       autofocus
+                                       type="text"
+                                       autocomplete="new-password"
+                                       id="goal-title"
+                                       v-focus="focused"
+                                       @focus="focused = true"
+                                       @blur="focused = false"
+                                />
+                                <!--<div class="input-group-addon">@</div>-->
+                                <button type="submit" class="btn btn-primary right-side">Submit</button>
+                            </div>
+                        </form>
+                        <!--<button type="submit" text="asdf">yes</button>-->
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-sm">
+                    <h2>Count: {{goals.length}}</h2>
+                    <div v-for="goal in goals">
+                        <p>
+                        <h3>{{goal}}</h3>
+                    </div>
+                </div>
+            </div>
+            <!--<div>{{$store.counter}}</div>-->
+            <!--<button @click="$store.commit('increment')">{{ $store.state.counter }}</button>-->
         </div>
-      </div>
-      <div class="row">
-        <div class="col-sm">
-          <span>New goal:</span>
-
-          <div class="goal-title-container">
-            <form class="form-inline goal-form"
-                  v-on:submit.prevent="createGoal"
-                  autocomplete="off">
-              <div class="input-group">
-                <label class="sr-only" for="inlineFormInput">Name</label>
-                <!-- these are to remove the autocomplete from Chrome -->
-                <input style="display:none" type="text" name="fakeusernameremembered"/>
-                <input style="display:none" type="password" name="fakepasswordremembered"/>
-                <input v-model="goalName"
-                       autofocus
-                       type="text"
-                       autocomplete="new-password"
-                       id="goal-title"
-                       v-focus="focused"
-                       @focus="focused = true"
-                       @blur="focused = false"
-                />
-                <!--<div class="input-group-addon">@</div>-->
-                <button type="submit" class="btn btn-primary right-side">Submit</button>
-              </div>
-            </form>
-            <!--<button type="submit" text="asdf">yes</button>-->
-          </div>
-        </div>
-      </div>
-
-
-      <div class="row">
-
-        <div class="col-sm">
-
-          </div>      <h2>Count: {{goals.length}}</h2>
-          <div v-for="goal in goals">
-            <p><h3>{{goal}}</h3>
-        </div>
-      </div>
-      <!--<div>{{$store.counter}}</div>-->
-      <!--<button @click="$store.commit('increment')">{{ $store.state.counter }}</button>-->
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
@@ -73,84 +73,86 @@
     },
     methods: {
       createGoal () {
-        this.$store.dispatch('addGoal', { payload: this.goalName })
+        this.$store.dispatch('addGoal', {name: this.goalName})
         this.goalName = ''
         document.getElementById('goal-title').focus()
       }
     },
-    computed: mapState(['goals'])
+    computed: {
+      ...mapState(['goals'])
+    }
   }
 </script>
 
 <style>
-  [class*='col-'] {
-    background: #EEE;
-  }
+    [class*='col-'] {
+        background: #EEE;
+    }
 
-  button {
-    font-size: 30pt;
-  }
+    button {
+        font-size: 30pt;
+    }
 
-  #goal-title {
-    font-size: 30pt;
-    width: 100%;
-    height: 100%;
-    border: 0;
-    padding: 0;
-    margin: 0;
-    border-radius: 5px 0 0 5px;
-  }
+    #goal-title {
+        font-size: 30pt;
+        width: 100%;
+        height: 100%;
+        border: 0;
+        padding: 0;
+        margin: 0;
+        border-radius: 5px 0 0 5px;
+    }
 
-  .goal-title-container {
-    /*border: 2px dashed white;*/
-    border-radius: 12px;
-    width: 80%;
-  }
+    .goal-title-container {
+        /*border: 2px dashed white;*/
+        border-radius: 12px;
+        width: 80%;
+    }
 
-  .container-fluid {
-    min-height: 100vh;
-    display: flex;
-    white-space: nowrap;
-  }
+    .container-fluid {
+        min-height: 100vh;
+        display: flex;
+        white-space: nowrap;
+    }
 
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
+    .title {
+        font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+        display: block;
+        font-weight: 300;
+        font-size: 100px;
+        color: #35495e;
+        letter-spacing: 1px;
+    }
 
-  .subtitle {
+    .subtitle {
 
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
+        font-weight: 300;
+        font-size: 42px;
+        color: #526488;
+        word-spacing: 5px;
+        padding-bottom: 15px;
 
-    /*justify-content: right;*/
-    /*align-items: right;*/
-    /*text-align: right;*/
-  }
+        /*justify-content: right;*/
+        /*align-items: right;*/
+        /*text-align: right;*/
+    }
 
-  .links {
-    padding-top: 15px;
-    font-size: 17pt;
-    text-align: left;
-  }
+    .links {
+        padding-top: 15px;
+        font-size: 17pt;
+        text-align: left;
+    }
 
-  .right-side {
-    justify-content: right;
-    align-items: right;
-    text-align: right;
-  }
+    .right-side {
+        justify-content: right;
+        align-items: right;
+        text-align: right;
+    }
 
-  .left-side {
-    justify-content: left;
-    align-items: left;
-    text-align: left;
-    background-color: lightblue;
-  }
+    .left-side {
+        justify-content: left;
+        align-items: left;
+        text-align: left;
+        background-color: lightblue;
+    }
 </style>
