e24ad6bf2e0a2edda1eafb0435424d9dc1eeaa41
Pavel Henkin <pavel.henkin@gmail.com>
2018-01-11 17:35:21 -0800

    removed config

--- local/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  google: {
-    clientId: '7655171871-2fgmpiqne0q2tr38g605aad8v35nglqg.apps.googleusercontent.com',
-    clientSecret: 'x0Ygh0I7RiiIB59NtfXS-NWM',
-    callbackUrl: 'http://localhost:3000/auth/login'
-  }
-}
